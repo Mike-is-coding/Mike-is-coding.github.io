e476e3cd45faeba748484f4ca72252472bf16e0a
Mike-is-coding <miguel.fierro750@gmail.com>
2023-01-13 14:12:05 -0800

    Basic portfolio page updated

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Background">
+      <div className='Landing-section'>
+        <header className='navbar'>
+          
+        </header>
+      </div>
+      <div className='About-me'>
+
+      </div>
+      <div className='Portfolio'>
+
+      </div>
     </div>
   );
 }
