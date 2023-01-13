08fa1d12d46b02bab64a97b50b43d7d3072b975c
Mike-is-coding <miguel.fierro750@gmail.com>
2023-01-13 15:02:03 -0800

    Deleted project section

--- src/App.js
+++ src/App.js
@@ -15,41 +15,9 @@ function App() {
     </div>
     <section id='welcome-section'>
       <h1 class='welcome-text'>Hey I'm Miguel</h1>
-      <p>Full Stack Developer</p>
+      <p>Front-End Developer</p>
     </section>
-    <section id='projects'>
-      <h2>These are some of my projects</h2>
-      <div class='projects-grid'>
-        <a class='project project-tile' href='https://codepen.io/mike-is-coding-the-styleful/full/vYjpoLb' target='_blank' rel='noreferrer'>
-          <img class='project-image' src='https://i.imgur.com/DumNbI9.png' alt='project' />
-          <p class='project-tile'>
-            <span class='code'>{"<"}</span>
-            Survey Form
-            <span class='code'>{"/>"}</span>
-          </p>
-        </a>
-      </div>
-      <div class='projects-grid'>
-        <a class='project project-tile' href='https://codepen.io/mike-is-coding-the-styleful/full/QWraeKE' target='_blank' rel='noreferrer'>
-          <img class='project-image' src='https://i.imgur.com/QZvJMAy.png' alt='project' />
-          <p class='project-tile'>
-            <span class='code'>{"<"}</span>
-            Tribute Page
-            <span class='code'>{"/>"}</span>
-          </p>
-        </a>
-      </div>
-      <div class='projects-grid'>
-        <a class='project project-tile' href='https://codepen.io/mike-is-coding-the-styleful/full/ZEovgmx' target='_blank' rel='noreferrer'>
-          <img class='project-image' src='https://i.imgur.com/8L8eUcG.png' alt='project' />
-          <p class='project-tile'>
-            <span class='code'>{"<"}</span>
-            Waving Penguin
-            <span class='code'>{"/>"}</span>
-          </p>
-        </a>
-      </div>
-    </section>
+    
     <section id='contact-me' class='contact-section'>
       <div class='contact-section-header'>
         <h2> Lets Work Together...</h2>
